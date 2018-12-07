99c2f074ece4c990d7ad14d2cdd10b9df1b19430
Hanh <buiduchanh312@gmail.com>
2018-12-07 15:06:57 +0700

    add script

--- utils/movefile.py
+++ utils/movefile.py
@@ -2,14 +2,12 @@ import cv2
 import glob
 import os
 import shutil
-Dir_img = '/home/buiduchanh/WorkSpace/Unet/Pytorch-UNet/data/data_rust/train_images'
-Dir_anno = '/home/buiduchanh/WorkSpace/Unet/Pytorch-UNet/data/data_rust/small_anno'
-Des_img = '/home/buiduchanh/WorkSpace/Unet/Pytorch-UNet/data/data_rust/small_image'
-anolist = sorted(glob.glob('{}/*'.format(Dir_anno)))
-for anno in anolist:
-    print(anno)
+from PIL import Image
+Dir_anno = '/home/buiduchanh/WorkSpace/Unet/data/melona/test/annotations'
+Des_anno = '/home/buiduchanh/WorkSpace/Unet/data/melona/test/annotation_tmp'
+annolist = sorted(glob.glob('{}/*'.format(Dir_anno)))
+for anno in annolist:
     basename = os.path.splitext(os.path.basename(anno))[0]
-    smallimage = os.path.join(Dir_img,basename + '.jpg')
-    # print(smallimage)
-    # exit()
-    shutil.move(smallimage, Des_img)
+    img = Image.open(anno)
+    img.save('{}/{}.jpg'.format(Des_anno,basename))
+
